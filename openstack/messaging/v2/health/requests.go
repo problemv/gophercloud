231c892986ffaa1c6a26b649e8746783c783d188
Jude Cross <jucross@blizzard.com>
2018-01-17 14:21:35 -0800

    Fix parameters in get for health

--- openstack/messaging/v2/health/requests.go
+++ openstack/messaging/v2/health/requests.go
@@ -2,12 +2,12 @@ package health
 
 import "github.com/gophercloud/gophercloud"
 
-func GetPing(client *gophercloud.ServiceClient, id string) (r GetResult) {
+func GetPing(client *gophercloud.ServiceClient) (r GetResult) {
 	_, r.Err = client.Get(pingURL(client), &r.Body, nil)
 	return
 }
 
-func GetHealth(client *gophercloud.ServiceClient, id string) (r GetResult) {
+func GetHealth(client *gophercloud.ServiceClient) (r GetResult) {
 	_, r.Err = client.Get(healthURL(client), &r.Body, nil)
 	return
 }
